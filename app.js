ab87b1e6d6aba706b9c48037d74d6d434a0c32d5
Hao Hu <hhu@thoughtworks.com>
2016-07-26 21:53:41 +0800

    improve routers and add ItemController

--- app.js
+++ app.js
@@ -3,21 +3,19 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
-const routes = require('./routes/index');
-const users = require('./routes/users');
+const route = require('./routes/route');
 
 const app = express();
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 
-app.use('/', routes);
-app.use('/users', users);
+route.setRoutes(app);
 
 // catch 404 and forward to error handler
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -28,7 +26,7 @@ app.use((req, res, next)=>{
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use((err, req, res, next)=>{
+  app.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -39,7 +37,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use((err, req, res, next)=>{
+app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
